174a0992e3fa20588220c44acd67e438478eb8ef
foreverwin_zzi <jsy11101@gmail.com>
2022-04-21 15:31:43 +0900

    feat: add game message utils

---
+++ src/main/java/racingcar/utils/ExceptionMessage.java
@@ -0,0 +1,18 @@
+package racingcar.utils;
+
+public enum ExceptionMessage {
+    CAR_NAME_LENGTH_TOO_LONG("[ERROR] 자동차 이름이 5자를 넘었습니다."),
+    CAR_POSITION_WRONG("[ERROR] 자동차의 위치는 0보다 커야 합니다."),
+    CAR_PARTICIPATION_OVER("[ERROR] 참여가능한 최대 자동차의 숫자는 1000개 입니다."),
+    CAR_DUPLICATE_PARTICIPATION("[ERROR] 중복된 자동차 이름이 있습니다.");
+
+    private String message;
+
+    ExceptionMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
